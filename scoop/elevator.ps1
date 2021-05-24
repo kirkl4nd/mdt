6e2fccb0c956c7427fb4f27b35bb195091aae1b3
kirkl4nd <kirkland_burrill@yahoo.com>
2021-05-24 11:02:21 -0400

    Delete elevator.ps1
    Removed elevator.ps1.
    Not needed, as commands during MDT are automatically run as local admin.

--- scoop/elevator.ps1
+++
@@ -1,5 +0,0 @@
-$user = "knight0\sccmservice" 
-$pwd1 = "Bell2282"
-$pwd = ($pwd1 | ConvertTo-SecureString)
-$Credential = New-Object System.Management.Automation.PSCredential $user, $pwd
-Start-Process powershell.exe -Credential $Credential -ArgumentList (".\install.ps1")
