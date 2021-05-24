9fa6f0bad78dce04bbf216cba6134819a6f3cbfe
kirkl4nd <77247639+kirkl4nd@users.noreply.github.com>
2021-05-24 08:43:23 -0400

    Create install-modules.ps1

---
+++ pip/install-modules.ps1
@@ -0,0 +1,10 @@
+# Python PIP libraries
+# DEPEND: Scoop Installed, Python Installed
+
+python3 -m pip install --upgrade pip
+pip install pylint
+pip3 install pillow
+pip3 install pygame
+pip3 install matplotlib
+pip3 install pandas
+pip3 install godirect[usb,ble]
