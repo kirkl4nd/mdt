0bda53429e172bef277ae384cf36fd48b2d2aedc
kirkl4nd <kirkland_burrill@yahoo.com>
2021-05-24 12:20:48 -0400

    Update ultimate-performance.ps1
    Finalized power script

--- power-performance/ultimate-performance.ps1
+++ power-performance/ultimate-performance.ps1
@@ -2,11 +2,27 @@
 # This will signifigantly speed up many computers.
 
 # Install ultimate performance plan
+# KIRK BURRILL, MAY 2021
+
+# Set plan to balanced
+$balanced = Get-CimInstance -Name root\cimv2\power -Class Win32_PowerPlan -Filter "ElementName = 'Balanced'"
+$balguid = ($balanced.InstanceID).Replace("Microsoft:PowerPlan\{","").Replace("}","")
+powercfg /setactive $balguid
+
+# Delete singular plan
+$p = Get-CimInstance -Name root\cimv2\power -Class win32_PowerPlan -Filter "ElementName = 'Ultimate Performance'"
+powercfg /d ([string]$p.InstanceID).Replace("Microsoft:PowerPlan\{","").Replace("}","")
+
+# Delete multiple plans
+foreach ($plan in $p){
+    powercfg /d ([string]$plan.InstanceID).Replace("Microsoft:PowerPlan\{","").Replace("}","")
+}
+
+# Create new plan
 powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
 
-# Locate and activate new plan
-$p = Get-CimInstance -Name root\cimv2\power -Class win32_PowerPlan -Filter "ElementName = 'Ultimate Performance'"
+$ultpower = Get-CimInstance -Name root\cimv2\power -Class Win32_PowerPlan -Filter "ElementName = 'Ultimate Performance'"
+$guid = ($ultpower.InstanceID).Replace("Microsoft:PowerPlan\{","").Replace("}","")
+powercfg /setactive $guid
 
-echo $p
-
-powercfg /setactive ([string]$p.InstanceID).Replace("Microsoft:PowerPlan\{","").Replace("}","")
+# END
