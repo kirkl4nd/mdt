f692403b97199d5745edd213d008a73bb53a76d6
kirkl4nd <kirkland_burrill@yahoo.com>
2021-05-24 11:10:48 -0400

    Update ultimate-performance.ps1
    Fixed power performance glitch

--- power-performance/ultimate-performance.ps1
+++ power-performance/ultimate-performance.ps1
@@ -2,8 +2,8 @@
 # This will signifigantly speed up many computers.
 
 # Install ultimate performance plan
-powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 
+powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
 
 # Locate and activate new plan
 $p = Get-CimInstance -Name root\cimv2\power -Class win32_PowerPlan -Filter "ElementName = 'Ultimate Performance'"
-powercfg /setactive ([String]$p.InstanceID).Replace("Microsoft:PowerPlan\{","").Replace("}","")
+powercfg /setactive ([string]$p.InstanceID).Replace("Microsoft:PowerPlan\{","").Replace("}","")
