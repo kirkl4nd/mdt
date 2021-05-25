c3662b4ebc4f08dbe29ea7493ef80bffe0ddc7f4
kirkl4nd <kirkland_burrill@yahoo.com>
2021-05-25 08:33:23 -0400

    Rename base script
    Fix name sceheme

---
+++ scoop/base-script.ps1
@@ -0,0 +1,12 @@
+# Scoop install script
+# Lab software
+scoop install git
+scoop bucket add extras
+scoop bucket add java
+scoop bucket add jetbrains
+scoop update *
+scoop install git sudo nano vlc winrar 7zip obs-studio sharex neofetch sublime-text googlechrome firefox audacity python openjdk --global
+scoop update *
+scoop uninstall lessmsi --global
+# Copy shortcuts
+Copy-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Scoop Apps\*" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs" -force -recurse
