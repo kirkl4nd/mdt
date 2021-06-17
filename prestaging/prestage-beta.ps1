578482eabaa5c89c400373fdb148e89bfc01f94d
kirkl4nd <kirkland_burrill@yahoo.com>
2021-06-17 13:12:31 -0400

    Updates

--- prestaging/prestage-beta.ps1
+++ prestaging/prestage-beta.ps1
@@ -1,5 +1,14 @@
+Clear
+Echo "Netboot Pre-Staging"
+Echo "-------------------"
+Echo " "
+Echo "This script will pre-stage devices for netbooting, and create computer objects in Active Directory."
+Echo "Enter device names and mac addresses. Be sure to move final objects to the proper OU!!!"
+Echo ""
 while($true) {
-	$name = Read-Host -Prompt "Enter computer name: "
-	[guid]$netbootguid = Read-Host -Prompt "Enter computer MAC32: "
+	$name = Read-Host -Prompt "Enter computer name "
+	$mac = Read-Host -Prompt "Enter computer mac address "
+	[guid]$netbootguid = "00000000000000000000"+$mac
 	new-adcomputer -Name $name -OtherAttributes @{'NetbootGUID'=$netbootguid;}
+	Echo "-------------------"
 }
